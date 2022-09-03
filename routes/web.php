67fc30d43038b95a7a333c6b1ef084bea6c15f2e
jacktea88 <jacktea@gmail.com>
2022-09-03 16:48:34 +0800

    try to add check middleware for laratrust admin panel but not working

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CheckController;
+use App\Http\Controllers\RolesController;
+use App\Http\Controllers\PermissionsController;
+use App\Http\Controllers\RolesAssignmentController;
+// use Laratrust\Http\Controllers\RolesController;
+// use Laratrust\Http\Controllers\PermissionsController;
+// use Laratrust\Http\Controllers\RolesAssignmentController;
+// namespace Laratrust\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +29,26 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
 // Route::middleware(['auth','HasPermission'])->group(function () {
 Route::middleware(['auth'])->group(function () {
 
     Route::get('/create',[CheckController::class, 'create']);
     // Route::get('/',[CheckController::class, 'index']);
     // Route::get('/home', [HomeController::class, 'index'])->name('home');
+
 });
+
+// todo: move to here but not working
+//not only login but also login as admin
+// Route::resource('/permissions', 'PermissionsController', ['as' => 'laratrust'])
+//     ->only(['index', 'create', 'store', 'edit', 'update'])->middleware(['role:Admin']);
+
+// Route::resource('/roles', 'RolesController', ['as' => 'laratrust'])->middleware(['role:Admin']);
+
+// Route::resource('/roles-assignment', 'RolesAssignmentController', ['as' => 'laratrust'])
+//     ->only(['index', 'edit', 'update'])->middleware(['role:Admin']);
+
 
 
 require __DIR__.'/auth.php';
