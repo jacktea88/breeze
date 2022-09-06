a802a70ebda4f643959da2f3197236556db57db6
jacktea88 <jacktea@gmail.com>
2022-09-06 23:15:36 +0800

    add laratrust controller from its package folder

---
+++ app/Http/Controllers/RolesAssignmentController.php
@@ -0,0 +1,109 @@
+<?php
+
+// namespace Laratrust\Http\Controllers;
+namespace App\Http\Controllers;
+
+use Laratrust\Helper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
+
+class RolesAssignmentController
+{
+    protected $rolesModel;
+    protected $permissionModel;
+    protected $assignPermissions;
+
+    public function __construct()
+    {
+        $this->rolesModel = Config::get('laratrust.models.role');
+        $this->permissionModel = Config::get('laratrust.models.permission');
+        $this->assignPermissions = Config::get('laratrust.panel.assign_permissions_to_user');
+    }
+
+    public function index(Request $request)
+    {
+        $modelsKeys = array_keys(Config::get('laratrust.user_models'));
+        $modelKey = $request->get('model') ?? $modelsKeys[0] ?? null;
+        $userModel = Config::get('laratrust.user_models')[$modelKey] ?? null;
+
+        if (!$userModel) {
+            abort(404);
+        }
+
+        return View::make('laratrust::panel.roles-assignment.index', [
+            'models' => $modelsKeys,
+            'modelKey' => $modelKey,
+            'users' => $userModel::query()
+                ->withCount(['roles', 'permissions'])   //它會被放在「{$relation}_count」這個欄位
+                ->simplePaginate(10),
+        ]);
+    }
+
+    public function edit(Request $request, $modelId)
+    {
+        $modelKey = $request->get('model');
+        $userModel = Config::get('laratrust.user_models')[$modelKey] ?? null;
+
+        if (!$userModel) {
+            Session::flash('laratrust-error', 'Model was not specified in the request');
+            return redirect(route('laratrust.roles-assignment.index'));
+        }
+
+        $user = $userModel::query()
+            ->with(['roles:id,name', 'permissions:id,name'])
+            ->findOrFail($modelId);
+
+        $roles = $this->rolesModel::orderBy('name')->get(['id', 'name', 'display_name'])
+            ->map(function ($role) use ($user) {
+                $role->assigned = $user->roles
+                ->pluck('id')
+                    ->contains($role->id);
+                $role->isRemovable = Helper::roleIsRemovable($role);
+
+                return $role;
+            });
+        if ($this->assignPermissions) {
+            $permissions = $this->permissionModel::orderBy('name')
+                ->get(['id', 'name', 'display_name'])
+                ->map(function ($permission) use ($user) {
+                    $permission->assigned = $user->permissions
+                        ->pluck('id')
+                        ->contains($permission->id);
+
+                    return $permission;
+                });
+        }
+
+
+        return View::make('laratrust::panel.roles-assignment.edit', [
+            'modelKey' => $modelKey,
+            'roles' => $roles,
+            'permissions' => $this->assignPermissions ? $permissions : null,
+            'user' => $user,
+        ]);
+    }
+
+    public function update(Request $request, $modelId)
+    {
+        $modelKey = $request->get('model');
+        $userModel = Config::get('laratrust.user_models')[$modelKey] ?? null;
+        dump($request);
+        if (!$userModel) {
+            Session::flash('laratrust-error', 'Model was not specified in the request');
+            return redirect()->back();
+        }
+
+        $user = $userModel::findOrFail($modelId);
+        $user->syncRoles($request->get('roles') ?? []);
+        dump($request->get('permissions'));
+        if ($this->assignPermissions) {
+            $user->syncPermissions($request->get('permissions') ?? []);
+        }
+        dd($request->get('roles'));
+
+        Session::flash('laratrust-success', 'Roles and permissions assigned successfully');
+        return redirect(route('laratrust.roles-assignment.index', ['model' => $modelKey]));
+    }
+}
