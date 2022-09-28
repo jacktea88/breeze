d2b40ce682c325a63d9ea798445598523256ac6c
jacktea88 <jacktea@gmail.com>
2022-09-28 14:10:14 +0800

    導入gmail發信功能(user填feedback後會發信給admin)

---
+++ app/Http/Controllers/Api/MailController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    //
+    public function sendMail(Request $request)
+    {
+        $data = $request->all();
+
+        //Laravel Mail 使用方法可參考：https://laravel.com/docs/5.1/mail#sending-mail
+        Mail::send('email.mail', $data, function ($message) use ($data) {
+            $message->from(ENV('MAIL_USERNAME', 'jacktea@gmail.com'), $data['name']);
+            $message->to(env('MAIL_SUPPORT', 'ttt@gmail.com'))->subject('3rd Feedback Mail');
+        });
+        return "success";
+    }
+}
---
+++ resources/views/email/mail.blade.php
@@ -0,0 +1,5 @@
+{{-- 收到的mail要呈現的內容 --}}
+
+<p>This is Feedback from {{ $name }}</p>
+<p>Sender mail is : {{ $email }}</p>
+<p>{{ $content }}</p>
