3da2722a4fb6a05fffc3c03b061190a9ba79620e
jacktea88 <jacktea@gmail.com>
2022-10-11 11:53:09 +0800

    修正4 crud的新增項目與back to home的網址連結

--- resources/views/DietGroup/show.blade.php
+++ resources/views/DietGroup/show.blade.php
@@ -26,7 +26,7 @@
                         </div>
                     </div>
 
-                    <div class="my-2 ml-2"><a href="/DietGroup" style="text-align: left;">Back to HOME</a></div>
+                    <div class="my-2 ml-2"><a href="{{ url('/DietGroup') }}" style="text-align: left;">Back to HOME</a></div>
 
 
 
