a74faeee0c197c5d9d74c16a4c68748d817fa00d
jacktea88 <jacktea@gmail.com>
2022-10-17 10:05:39 +0800

    修改feedback mail title to yumeal feedback

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -16,7 +16,7 @@ class MailController extends Controller
         //Laravel Mail 使用方法可參考：https://laravel.com/docs/5.1/mail#sending-mail
         Mail::send('email.mail', $data, function ($message) use ($data) {
             $message->from(ENV('MAIL_USERNAME', 'jacktea@gmail.com'), $data['name']);
-            $message->to(env('MAIL_SUPPORT', 'ttt@gmail.com'))->subject('3rd Feedback Mail');
+            $message->to(env('MAIL_SUPPORT', 'ttt@gmail.com'))->subject('Yumeal Feedback Mail');
         });
         return "success";
     }
